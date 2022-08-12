9ec514c171af73516e0829ccb83fee42cf749667
Leon Guenther <leonsechteemailadresse@gmail.com>
2022-08-13 00:26:48 +1000

    crude

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
-import { env } from "./src/env/server.mjs";
-
 /**
  * Don't be scared of the generics here.
  * All they do is to give us autocompletion when using this.
  *
- * @template {import('next').NextConfig} T
+ * @template {import("next").NextConfig} T
  * @param {T} config - A generic parameter that flows through to the return type
- * @constraint {{import('next').NextConfig}}
+ * @constraint {{import("next").NextConfig}}
  */
 function defineNextConfig(config) {
   return config;
@@ -15,4 +13,5 @@ function defineNextConfig(config) {
 export default defineNextConfig({
   reactStrictMode: true,
   swcMinify: true,
+  images: { domains: ["c1.scryfall.com"] }
 });
