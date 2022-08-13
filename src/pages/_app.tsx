67840ee96b1c2e04eddfe10da6174174ec0c6ba1
Leon Guenther <leonsechteemailadresse@gmail.com>
2022-08-13 23:56:13 +1000

    fetching pictures to show loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
 import '../styles/globals.css';
 import type { AppType } from 'next/dist/shared/lib/utils';
 
+import { useState } from 'react';
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 };
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,46 @@ import { useEffect, useState } from 'react';
 import _ from 'lodash';
 import all_planes from '../data/all_planes.json';
 import { Card } from 'scryfall-api';
-import Image from 'next/image';
 import { SetRequired } from 'type-fest';
+import { useQuery } from '@tanstack/react-query';
 
 type GoodCard = SetRequired<Omit<Card, 'prices'>, 'image_uris'>;
 
 const planes = _.shuffle(all_planes.cards) as GoodCard[];
+const planeAspectRatio = 974 / 670;
+const planeAspectRatio90degRot = 670 / 974;
 
 function splitFirst<T>(array: T[]): [T, T[]] {
   const head = _.head(array);
   if (head === undefined)
     throw Error('Trying to split array of length 0: ' + array.toString());
-
   return [head, _.tail(array)];
+}
+
+function PlaneCard({ card }: { card: GoodCard }) {
+  const { isLoading, error, data } = useQuery([card.name], () =>
+    fetch(card?.image_uris.border_crop).then(async (res) =>
+      URL.createObjectURL(await res.blob())
+    )
+  );
+
+  if (isLoading) return <div>Walking...</div>;
+
+  if (error) return <div>An error has occurred: + {error.message}</div>;
+
+  return (
+    <div className={'aspect-[974/670] overflow-hidden'}>
+      <img
+        className={
+          'relative left-full aspect-[670/974] w-[calc(100%*(670/974))] origin-top-left rotate-90 object-fill'
+        }
+        src={data}
+        alt={'planes'}
+        width={670}
+        height={974}
+      />
+    </div>
+  );
 }
 
 export default function App() {
@@ -56,18 +83,7 @@ export default function App() {
       >
         {deck.length < 1 ? 'shuffle' : 'walk'}
       </button>
-      {topCard && (
-        <Image
-          className={'rotate-90'}
-          src={topCard.image_uris.border_crop}
-          alt={'planes'}
-          layout="responsive"
-          width={'100%'}
-          height={'100%'}
-          objectFit="contain"
-          unoptimized
-        />
-      )}
+      {topCard ? <PlaneCard card={topCard} /> : <div>No cards :(</div>}
     </>
   );
 }
