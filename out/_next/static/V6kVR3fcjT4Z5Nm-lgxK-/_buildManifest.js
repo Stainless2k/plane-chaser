964b78848eab8c1e2e0045bb6521631729903846
Leon Guenther <leonsechteemailadresse@gmail.com>
2022-08-14 18:26:09 +1000

    removed out

--- out/_next/static/V6kVR3fcjT4Z5Nm-lgxK-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/29107295-313698bcfedf7960.js","static/chunks/pages/index-bb1d7a3669886580.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
