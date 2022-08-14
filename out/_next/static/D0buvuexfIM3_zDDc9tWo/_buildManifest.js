8b78295a387efc0dbca64b4a5a621d302aa84d8e
Leon Guenther <leonsechteemailadresse@gmail.com>
2022-08-14 16:15:25 +1000

    Deploy

---
+++ out/_next/static/D0buvuexfIM3_zDDc9tWo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/29107295-313698bcfedf7960.js","static/chunks/pages/index-bb1d7a3669886580.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
