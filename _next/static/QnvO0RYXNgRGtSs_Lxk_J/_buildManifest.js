8398d5fe750b6c146df0462484bb90a3acc31c46
Leon Guenther <leonsechteemailadresse@gmail.com>
2022-08-18 01:04:27 +1000

    Updates

---
+++ _next/static/QnvO0RYXNgRGtSs_Lxk_J/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/29107295-313698bcfedf7960.js","static/chunks/pages/index-916012a720e3e0e7.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
