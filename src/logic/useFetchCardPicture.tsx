f56db8e71bf380f85ff108a281c8d06b896b249c
Leon Guenther <leonsechteemailadresse@gmail.com>
2023-05-19 02:30:55 +0200

    'fixed' cors shit

--- src/logic/useFetchCardPicture.tsx
+++ src/logic/useFetchCardPicture.tsx
@@ -4,8 +4,8 @@ import { useQuery } from '@tanstack/react-query';
 //fetch card pictures from scryfall, mainly used to show loading state
 export function useFetchCardPicture(card: GoodCard | undefined) {
   return useQuery<string, Error>([card?.name], () =>
-    fetch(card?.image_uris.border_crop ?? '').then(async (res) =>
-      URL.createObjectURL(await res.blob())
+    fetch('https://corsproxy.io/?' + card?.image_uris.border_crop).then(
+      async (res) => URL.createObjectURL(await res.blob())
     )
   );
 }
